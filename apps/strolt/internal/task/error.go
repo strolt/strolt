38f3d4d03a1676085c0bcf05359d67d980511463
Alexander Svinarev <shibanet0@gmail.com>
2022-12-27 01:39:55 +0300

    feat(strolt): remove unused logic

--- apps/strolt/internal/task/error.go
+++
@@ -1,7 +0,0 @@
-package task
-
-import "fmt"
-
-func errorAlreadyWorking(serviceName string, taskName string) error {
-	return fmt.Errorf("task '%s' for service '%s' already working", taskName, serviceName)
-}
