c1aa8cc8ffd64e49c7bb92e39120b61cab1aa52d
Alexander Svinarev <shibanet0@gmail.com>
2022-12-30 21:59:14 +0300

    fix(strolt): restic restore

--- apps/strolt/internal/driver/destination/restic/restore.go
+++ apps/strolt/internal/driver/destination/restic/restore.go
@@ -7,7 +7,11 @@ import (
 )
 
 func (i *Restic) Restore(ctx context.Context, snapshotID string) error {
-	cmd := exec.Command(i.getBin(), "restore", snapshotID, "--target", ctx.WorkDir)
+	var args []string
+	args = append(args, i.getGlobalFlags()...)
+	args = append(args, "restore", snapshotID, "--target", ctx.WorkDir)
+
+	cmd := exec.Command(i.getBin(), args...)
 
 	env, err := i.getEnv()
 	if err != nil {
@@ -18,7 +22,7 @@ func (i *Restic) Restore(ctx context.Context, snapshotID string) error {
 
 	i.logger.Debug(cmd.String())
 
-	output, err := cmd.Output()
+	output, err := startCmd(cmd)
 	if err != nil {
 		i.logger.Error(err)
 		return err
