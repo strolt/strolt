a7b49ab97bb769bf005d2dad8507bd2d26bca024
Alexander Svinarev <shibanet0@gmail.com>
2023-02-23 12:21:53 +0300

    fix: build platform

--- scripts/get_platform.sh
+++ scripts/get_platform.sh
@@ -1,11 +1,19 @@
 architecture=""
-case $(uname -m) in
-    i386)   architecture="386" ;;
-    i686)   architecture="386" ;;
-    x86_64) architecture="amd64" ;;
-    arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
-esac
 
 platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+if [ "$platform" == "darwin" ]; then
+    case $(uname -m) in
+        x86_64) architecture="amd64" ;;
+        arm64) architecture="arm64" ;;
+    esac
+else
+    case $(uname -m) in
+        i386)   architecture="386" ;;
+        i686)   architecture="386" ;;
+        x86_64) architecture="amd64" ;;
+        arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    esac
+fi
 
 echo ""$platform"_"$architecture""
